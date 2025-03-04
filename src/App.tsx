e5119b86a8010e856e1f1e49b8f9ce24031612a3
iamwhoami <62898834+Roshan-Kumar-Sharma@users.noreply.github.com>
2025-03-04 20:56:26 +0530

    complete redux counter

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux';
+import './App.css';
+import { RootState } from './app/store';
+import { increment, decrement } from './app/counterSlice';
+
+function App() {
+  const counter = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="text-6xl font-bold mb-8">{counter}</div>
+      <div className="space-x-4">
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
